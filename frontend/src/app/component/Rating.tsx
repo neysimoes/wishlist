38aff15614d5d3e379f1d7ffc7f05c550d2938d8
Ney Simões <ney.junior@swift.com.br>
2024-09-02 13:32:22 -0300

    fix: rating warning on console

--- frontend/src/app/component/Rating.tsx
+++ frontend/src/app/component/Rating.tsx
@@ -5,14 +5,31 @@ type RatingProps = {
 }
 
 const Rating = ({ rating }: RatingProps) => {
+  if (rating > 5) {
+    rating = 5
+  }
+
+  if (!rating || rating < 0) {
+    rating = 0
+  }
+
   const fullStars = Math.floor(rating)
   const hasHalfStar = rating % 1 >= 0.5
   const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0)
   return (
-    <div className="flex flex-row">
-      {Array(fullStars).fill(<StarFill className="fill-yellow-400" />)}
+    <div className="flex flex-row text-base items-center">
+      {Array(fullStars)
+        .fill('')
+        .map((_, index) => (
+          <StarFill className="fill-yellow-400" key={index} />
+        ))}
       {hasHalfStar && <StarHalf className="fill-yellow-400" />}
-      {Array(emptyStars).fill(<Star className="fill-yellow-400" />)}
+      {Array(emptyStars)
+        .fill('')
+        .map((_, index) => (
+          <Star className="fill-yellow-400" key={index} />
+        ))}
+      <span className="text-sm">{rating}</span>
     </div>
   )
 }
